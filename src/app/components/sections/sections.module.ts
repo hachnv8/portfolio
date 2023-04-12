bdc306c6c0af7e94cc23c59285300a7ecc040b29
hachnv8 <hachnv8@gmail.com>
2023-04-12 14:03:36 +0700

    initialize project

---
+++ src/app/components/sections/sections.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LightboxModule } from 'ngx-lightbox';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { WorksComponent } from './works/works.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { GalleryComponent } from './gallery/gallery.component';
+import { UiModalComponent } from './ui-modal/ui-modal.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NguCarouselModule } from '@ngu/carousel';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    WorksComponent,
+    BlogComponent,
+    ContactComponent,
+    GalleryComponent,
+    UiModalComponent
+  ],
+  exports: [
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    WorksComponent,
+    BlogComponent,
+    ContactComponent,
+    NgbModule
+  ],
+  imports: [CommonModule, LightboxModule, NgImageSliderModule, IvyCarouselModule, NguCarouselModule, NgbModule]
+})
+export class SectionsModule {}
